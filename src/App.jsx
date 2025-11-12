6fd6ea2e770286d15afb89e6cd788a299e0e1937
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-11-12 12:46:47 +0100

    add the tow links and select one link

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
+    // const url = "https://lanciweb.github.io/demo/api/actresses/";
+    const url = "https://lanciweb.github.io/demo/api/actors/";
+
 
   }, []);
   return (
