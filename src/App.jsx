aa24639e2548be80beca5f81dfa884c161f58636
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-11-12 12:54:43 +0100

    add the usestate for the actors

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
 
 function App() {
+  const [actors, setActors] = useState([]);
+
   useEffect(() => {
-    // const url = "https://lanciweb.github.io/demo/api/actresses/";
     const url = "https://lanciweb.github.io/demo/api/actors/";
 
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
         console.log("Lista attori:", data);
+        setActors(data);
       })
       .catch((error) => {
         console.error("Errore nel recupero dei dati:", error);
       });
   }, []);
+
   return (
     <>
-
 
     </>
   )
